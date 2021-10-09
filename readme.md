ecadea26e53d5500f229d373ddede9e5c8232000
f0rget <volodymyr.fialko@gmail.com>
2021-10-09 18:11:56 +0200

    Implement ability to insert transition between tic-toks

--- readme.md
+++ readme.md
@@ -6,6 +6,34 @@ Download tic-toc videos and merge them into one video
 - [TikTokApi](https://dteather.com/TikTok-Api/docs/TikTokApi.html)
 - ffmpeg
 
+
+## Usage Examples
+
+```console
+$ ./tor-tik.py --help
+
+ ______   ___   ____                  ______  ____  __  _
+|      T /   \ |    \                |      Tl    j|  l/ ]
+|      |Y     Y|  D  )     _____     |      | |  T |  ' /
+l_j  l_j|  O  ||    /     |     |    l_j  l_j |  | |    \
+  |  |  |     ||    \     l_____j      |  |   |  | |     Y
+  |  |  l     !|  .  Y                 |  |   j  l |  .  |
+  l__j   \___/ l__j\_j                 l__j  |____jl__j\_j
+
+usage: tor-tik.py [-h] -i INPUT -o OUTPUT [-t TRANSITION]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input list file of tic-toks
+  -o OUTPUT, --output OUTPUT
+                        Output directory, where will stored all the results
+  -t TRANSITION, --transition TRANSITION
+                        Transition video(will be inserted after each tic-tok)
+
+$ ./tor-tik.py -i tt_list_example.txt -o downloads
+```
+
 ## Links
 - https://medium.com/analytics-vidhya/download-tiktoks-with-python-dcbd79a5237f
 - http://www.patorjk.com/software/taag/#p=display&f=Crawford&t=tor%20-%20tik
@@ -13,5 +41,5 @@ Download tic-toc videos and merge them into one video
 ## TODO
 - [x] Download tt videos from list
 - [x] Merge them together
-- [ ] Insert some short 'scene changer' between them
+- [x] Insert some short transition between them
 - [ ] Generate some sort of time codes(separate or bake into a video)
--- tor-tik.py
+++ tor-tik.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import shutil
 import sys
 import subprocess
 
@@ -23,7 +24,7 @@ def url2id(url):
     return int(url.split("/video/")[1].split("?")[0])
 
 def fname_from_tid(download_dir, tid):
-    return f"{download_dir}/{tid}.mp4"
+    return os.path.join(download_dir, f"{tid}.mp4")
 
 def download(tid, out_file):
     tt = api.get_tiktok_by_id(tid) # construct tic-tok dict
@@ -37,23 +38,55 @@ def get_urls_from_list(filename):
             if line.startswith("https://www.tiktok.com"):
                 yield line
 
-def gen_ffmpeg_config_file(out_files, download_dir):
+def intersperse(lst, item):
+    """
+    Insert item between each element it lst
+    """
+    result = [item] * (len(lst) * 2 - 1)
+    result[0::2] = lst
+    return result
+
+def gen_ffmpeg_config_file(out_files, out_dir):
     ff_lines = []
     for of in out_files:
         ff_lines.append(f"file '{os.path.basename(of)}'\n")
-        # XXX: separator have some issues with merging, probably related to
-        # the resolution or format
-        # ff_lines.append("file 'separator.mkv'\n")
 
-    ff_config_full_path = f"{download_dir}/{FFMEG_CONFIG}"
+    ff_config_full_path = os.path.join(out_dir, FFMEG_CONFIG)
     with open(ff_config_full_path, 'w') as f:
             f.writelines(ff_lines)
     return ff_config_full_path
 
+def ffmpeg_convert_to_mts(inputfile):
+    outfile = inputfile.replace(".mp4", ".mts")
+    subprocess.check_output(["ffmpeg", "-i", inputfile, "-y", "-q", "0",
+        outfile])
+    return outfile
+
+def ffmpeg_concat(ffmpeg_config, out_dir):
+    subprocess.check_output(["ffmpeg", "-f", "concat", "-i", ffmpeg_config,
+        "-y", "-c", "copy", f"{out_dir}/out.mts"])
+
+def concat_all(files, out_dir, transition):
+    # convert all to mts format to concatenate it later without any problems,
+    # XXX: not sure this is the best way to do this:
+    # https://video.stackexchange.com/questions/15468/non-monotonous-dts-on-concat-ffmpeg
+    out_files = [ffmpeg_convert_to_mts(f) for f in files]
+    if transition is not None:
+        transition = ffmpeg_convert_to_mts(transition)
+        out_files = intersperse(out_files, transition)
+    ffmpeg_config = gen_ffmpeg_config_file(out_files, out_dir)
+    ffmpeg_concat(ffmpeg_config, out_dir)
 
 def main(config):
     tids = [url2id(url) for url in get_urls_from_list(config.input)]
     download_dir = config.output
+    transition = None
+
+    if config.transition is not None:
+        transition = os.path.join(download_dir,
+                os.path.basename(config.transition))
+        shutil.copyfile(config.transition, transition)
+        # subprocess.check_output(["cp", config.transition, download_dir])
 
     print(f"Number of tic-toks: {len(tids)}")
     print("Downloading...")
@@ -72,9 +105,7 @@ def main(config):
             print(f"{tid} fail")
 
     print("generating full video...")
-    ffmpeg_file = gen_ffmpeg_config_file(out_files, download_dir)
-    subprocess.check_output(["ffmpeg", "-f", "concat", "-i", ffmpeg_file,
-        "-y", "-c", "copy", f"{download_dir}/out.mp4"])
+    concat_all(out_files, download_dir, transition)
 
 def parse_args(argv):
     parser = argparse.ArgumentParser()
@@ -82,6 +113,9 @@ def parse_args(argv):
         help="Input list file of tic-toks", type=str)
     parser.add_argument("-o", "--output", required=True, dest="output",
         help="Output directory, where will stored all the results", type=str)
+    parser.add_argument("-t", "--transition", required=False,
+            dest="transition", type=str, default=None,
+            help="Transition video(will be inserted after each tic-tok)")
     args = parser.parse_args()
     if not os.path.exists(args.output):
         os.mkdir(args.output)
@@ -89,6 +123,6 @@ def parse_args(argv):
     return args
 
 if __name__ == "__main__":
-    config = parse_args(sys.argv)
     print(logo)
+    config = parse_args(sys.argv)
     main(config)
